b08b615bb360b373cd968129dbdb835ed3ff1030
Terrence Lun <terrence.lun@gmail.com>
2014-08-02 15:06:14 -0400

    Added TLTest

---
+++ TLTest.h
@@ -0,0 +1,13 @@
+//
+//  TLTest.h
+//  Git Test
+//
+//  Created by Terrence Lun on 2014-08-02.
+//  Copyright (c) 2014 Terrence Lun. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface TLTest : NSObject
+
+@end
---
+++ TLTest.m
@@ -0,0 +1,13 @@
+//
+//  TLTest.m
+//  Git Test
+//
+//  Created by Terrence Lun on 2014-08-02.
+//  Copyright (c) 2014 Terrence Lun. All rights reserved.
+//
+
+#import "TLTest.h"
+
+@implementation TLTest
+
+@end
